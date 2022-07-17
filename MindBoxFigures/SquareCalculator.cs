25a53bf21552ce4e3803d2cc623124956129aa51
Koldrif <vorobiov.777@yandex.ru>
2022-07-17 19:05:10 +0300

    Initial

---
+++ MindBoxFigures/SquareCalculator.cs
@@ -0,0 +1,15 @@
+﻿namespace MindBoxFigures;
+
+public static class SquareCalculator
+{
+    public static double CalcCircleSquare(double radius)
+    {
+        return Math.PI * radius * radius;
+    }
+
+    public static double CalcTriangleSquare(double a, double b, double c)
+    {
+        double semiPerimeter = (a + b + c) / 2;
+        return Math.Sqrt(semiPerimeter * (semiPerimeter - a) * (semiPerimeter - b) * (semiPerimeter - c));
+    }
+}
